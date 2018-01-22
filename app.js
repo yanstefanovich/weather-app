7f926bb03853e8e11bf3fbe56e9d299da3a6ab05
Yan <yan.stefanovich@ryerson.ca>
2018-01-22 14:39:56 -0500

    Added Error Handling

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(argv.address),
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (error){
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find the provided address.');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  } else {
+    console.log('Something went wrong');
+  }
 });
