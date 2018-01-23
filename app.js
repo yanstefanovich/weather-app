b5c72179fd94a945645a88dfac7468fcbd48f2ca
Yan <yan.stefanovich@ryerson.ca>
2018-01-23 16:24:40 -0500

    Refactored geocode request from google API into a seperate module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
 .options({
@@ -14,19 +15,10 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(argv.address),
-  json: true
-}, (error, response, body) => {
-  if (error){
-    console.log('Unable to connect to Google servers.');
-  } else if (body.status === 'ZERO_RESULTS'){
-    console.log('Unable to find the provided address.');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
   } else {
-    console.log('Something went wrong');
+    console.log(JSON.stringify(results, undefined, 2));
   }
 });
---
+++ geocode/geocode.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+  request({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURIComponent(address),
+    json: true
+  }, (error, response, body) => {
+    if (error){
+      callback('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS'){
+      callback('Unable to find the provided address.');
+    } else if (body.status === 'OK') {
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        lattitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
+    } else {
+      callback('Something went wrong, try again');
+    }
+  });
+};
+module.exports = {
+  geocodeAddress
+};
